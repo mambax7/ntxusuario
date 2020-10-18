f39ebc7fb7fa9f9495d5626b58be5ae7e42e50c3
mambax7 <mambax7@gmail.com>
2020-10-18 04:04:47 -0400

    assets, logoModule.png

--- xoops_version.php
+++ xoops_version.php
@@ -1,16 +1,26 @@
 <?php
 
-$modversion['name']        = _MD_NOMBRE;
-$modversion['version']     = '1.0';
-$modversion['description'] = _MD_DESCRIPCION;
-$modversion['credits']     = 'http://www.natxocc.com';
-$modversion['author']      = 'NatxoCC';
-$modversion['status']      = 'RC2';
-$modversion['help']        = '';
-$modversion['license']     = '';
-$modversion['official']    = 0;
-$modversion['image']       = 'ntxusuario_logo.png';
-$modversion['dirname']     = 'ntxusuario';
+$moduleDirName      = basename(__DIR__);
+$moduleDirNameUpper = mb_strtoupper($moduleDirName);
+
+$modversion['name']                = _MD_NOMBRE;
+$modversion['version']             = '2.01';
+$modversion['description']         = _MD_DESCRIPCION;
+$modversion['credits']             = 'http://www.natxocc.com';
+$modversion['author']              = 'NatxoCC';
+$modversion['status']              = 'Beta 1';
+$modversion['help']                = '';
+$modversion['license']             = '';
+$modversion['official']            = 0;
+$modversion['dirname']             = $moduleDirName;
+$modversion['modicons16']          = 'assets/images/icons/16';
+$modversion['modicons32']          = 'assets/images/icons/32';
+$modversion['image']               = 'assets/images/logoModule.png';
+$modversion['module_website_url']  = 'https://xoops.org';
+$modversion['module_website_name'] = 'XOOPS';
+$modversion['min_php']             = '7.1';
+$modversion['min_xoops']           = '2.5.10';
+$modversion['min_admin']           = '1.2';
 
 //Menu
 $modversion['hasMain'] = 0;
